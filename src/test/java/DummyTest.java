cd34dd338b4edcfbb0f3a1ff04a89598c1d47c35
Henrique Cassús <henrique.cassus@meuspedidos.com.br>
2016-01-31 22:31:24 -0200

    initial commit
    First commit to our remote repo

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by hcassus on 31/01/2016.
+ */
+public class DummyTest {
+
+    @Test
+    public void dummyTest(){
+        Assert.assertTrue(true);
+    }
+}
